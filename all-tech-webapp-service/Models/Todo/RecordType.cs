597b284a1edc1e13fb74e18cdd19768827ac83b8
Utkarsh Behre <utkarshbehre@microsoft.com>
2024-03-17 21:29:01 -0700

    enum to string converter for db

--- all-tech-webapp-service/Models/Todo/RecordType.cs
+++ all-tech-webapp-service/Models/Todo/RecordType.cs
@@ -1,5 +1,9 @@
-﻿namespace all_tech_webapp_service.Models.Todo
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace all_tech_webapp_service.Models.Todo
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum RecordType
     {
         TodoItem,
