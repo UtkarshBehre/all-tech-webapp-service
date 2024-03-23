b61e837b46d7588429f8ced003f039bf368c633b
Utkarsh Behre <utkarshbehre@microsoft.com>
2024-03-23 14:00:56 -0700

    models json property

--- all-tech-webapp-service/Models/Todo/AllItemsResponse.cs
+++ all-tech-webapp-service/Models/Todo/AllItemsResponse.cs
@@ -1,12 +1,15 @@
 ﻿using all_tech_webapp_service.Models.Todo.Group;
 using all_tech_webapp_service.Models.Todo.Item;
+using Newtonsoft.Json;
 
 namespace all_tech_webapp_service.Models.Todo
 {
     public class AllItemsResponse
     {
+        [JsonProperty("todoItems")]
         public IEnumerable<TodoItemResponse> TodoItems { get; set; }
 
+        [JsonProperty("groupsMap")]
         public Dictionary<string, TodoGroupResponse> GroupsMap { get; set; }
 
         public AllItemsResponse()
