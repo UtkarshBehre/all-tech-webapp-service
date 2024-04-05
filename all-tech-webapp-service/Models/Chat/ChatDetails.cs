ab48e39c4ebd28ba1701b5498cb6be696c732e8d
Utkarsh Behre <utkarshbehre@microsoft.com>
2024-04-05 13:05:43 -0700

    add json property in chat details

--- all-tech-webapp-service/Models/Chat/ChatDetails.cs
+++ all-tech-webapp-service/Models/Chat/ChatDetails.cs
@@ -1,8 +1,12 @@
-﻿namespace all_tech_webapp_service.Models.Chat
+﻿using Newtonsoft.Json;
+
+namespace all_tech_webapp_service.Models.Chat
 {
     public class ChatDetails
     {
+        [JsonProperty("userName")]
         public string UserName { get; set; }
+        [JsonProperty("chatRoom")]
         public string ChatRoom { get; set; }
     }
 }
