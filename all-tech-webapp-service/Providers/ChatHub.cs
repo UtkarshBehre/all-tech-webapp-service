df2dafabe0da25e35a43d633d19dad5c3654c0fd
Utkarsh Behre <utkarshbehre@microsoft.com>
2024-04-05 14:57:47 -0700

    Add SendMessage method to ChatHub

--- all-tech-webapp-service/Providers/ChatHub.cs
+++ all-tech-webapp-service/Providers/ChatHub.cs
@@ -5,6 +5,13 @@ namespace all_tech_webapp_service.Providers
 {
     public class ChatHub : Hub
     {
+        public Dictionary<string, ChatDetails> connectionIdTochatDetails;
+
+        public ChatHub()
+        {
+            connectionIdTochatDetails = new Dictionary<string, ChatDetails>();
+        }
+
         public async Task JoinChat(ChatDetails chatDetails)
         {
             await Clients.All.SendAsync("ReceiveMessage", "admin", $"{chatDetails.UserName} has joined." );
@@ -13,8 +20,20 @@ namespace all_tech_webapp_service.Providers
         public async Task JoinSpecificChatRoom(ChatDetails chatDetails)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, chatDetails.ChatRoom);
+
+            connectionIdTochatDetails.Add(Context.ConnectionId, chatDetails);
+
             await Clients.Group(chatDetails.ChatRoom)
-                .SendAsync("ReceiveMessage", "admin", $"{chatDetails.UserName} has joined {chatDetails.ChatRoom}.");
+                .SendAsync("ReceiveSpecificMessage", "admin", $"{chatDetails.UserName} has joined {chatDetails.ChatRoom}.");
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if (connectionIdTochatDetails.TryGetValue(Context.ConnectionId, out ChatDetails chatDetails))
+            {
+                await Clients.Group(chatDetails.ChatRoom)
+                    .SendAsync("ReceiveSpecificMessage", chatDetails.UserName, message);
+            }
         }
     }
 }
