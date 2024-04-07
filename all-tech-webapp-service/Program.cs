a16d93662d945f6eaf62d400b1086367793cf9e2
Utkarsh Behre <utkarshbehre@microsoft.com>
2024-04-07 08:46:47 -0700

    Update hub pathing to match classname with Hub

--- all-tech-webapp-service/Program.cs
+++ all-tech-webapp-service/Program.cs
@@ -75,7 +75,7 @@ namespace all_tech_webapp_service
 
             app.MapControllers();
 
-            app.MapHub<TodoHub>("/hub");
+            app.MapHub<TodoHub>("/Todo");
 
             app.UseCors("AllTechApp");
 
